7a13bfa8a3a83eaccbd693626b2267c3d36a41e2
SamrithaShree <iamsamraja1111@gmail.com>
2025-05-04 08:40:09 +0530

    Update class.cpp

--- Lab02/class.cpp
+++ Lab02/class.cpp
@@ -1,5 +1,6 @@
+//program to calculate areas of 2D/3D shapes
 #include<stdio.h>
-//class containing
+
 class ShapeCalculator{
     private:
     float length,breadth,height;
@@ -11,35 +12,45 @@ class ShapeCalculator{
         breadth=0.0;
         height=0.0;
     }
+
+    // Get length input from user
     void getLength(){
         printf("Enter the length: \n");
         scanf("%f",&length);
     }
+
+    // Get breadth input from user
     void getBreadth(){
         printf("Enter the breadth: \n");
         scanf("%f",&breadth);
     }
+
+    // Get height input from user
     void getHeight(){
         printf("Enter the height: \n");
         scanf("%f",&height);
     }
 
+    // Calculate and display area of a square
     void SquareArea(){
         getLength();
         printf("The area of the square is %.2f\n",length*length);
     }
 
+    // Calculate and display surface area of a cube
     void CubeArea(){
         getLength();
         printf("The area of the cube is %.2f\n",6*length*length);
     }
 
+    // Calculate and display area of a rectangle
     void rectangleArea(){
        getLength();
        getBreadth();
        printf("The area of the rectangle is %.2f\n",length*breadth);
     }
 
+    // Calculate and display surface area of a cuboid
     void CuboidArea(){
         getLength();
         getBreadth();
@@ -51,7 +62,7 @@ class ShapeCalculator{
 int main(){
     ShapeCalculator Calc;
     int option;
-    //printing Menu
+
     while(1){
         printf("Menu:\n");
         printf("1.Square\n");
